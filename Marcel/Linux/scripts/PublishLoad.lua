acb293b84f0b83a6954da5f45459d23b9da31e8a
destroyedlolo <l_faillie@yahoo.com>
2024-09-15 00:30:27 +0200

    Publish memory

--- Marcel/Linux/scripts/PublishLoad.lua
+++ Marcel/Linux/scripts/PublishLoad.lua
@@ -10,8 +10,6 @@ function PublishLoad( section_name )
 
 	local topic = 'Load/' .. Marcel.Hostname() ..'/'
 
-print(">>>>", topic)
-
 	Marcel.MQTTPublish( topic .. '1', r1)
 	Marcel.MQTTPublish( topic .. '5', r5)
 	Marcel.MQTTPublish( topic .. '10', r10)
