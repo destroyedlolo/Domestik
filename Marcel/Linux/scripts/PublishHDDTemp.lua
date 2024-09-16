587307e53c9b5a222f63cf1b8c1ac4a0fda65fae
destroyedlolo <l_faillie@yahoo.com>
2024-09-16 23:35:37 +0200

    Add hddtemp

---
+++ Marcel/Linux/scripts/PublishHDDTemp.lua
@@ -0,0 +1,30 @@
+-- Publish disks temperature
+-- 
+--
+
+function PublishHDDTemp( section_name )
+	local socket = require("socket")
+
+	local clt = socket.connect( "localhost", 7634 )
+	if not clt then
+		return nil
+	end
+	local s, err = clt:receive('*a')
+	clt:close()
+
+	local res = {}
+	s=s.."|"
+	for mnt,tp,temp,unit in s:gmatch("|(.-)|(.-)|(.-)|(.-)|") do
+		temp = tonumber(temp)
+		if temp then
+			res[ mnt ] = temp
+		end
+	end
+
+	local topic = 'Machines/' .. Marcel.Hostname() ..'/HDDTemp/'
+
+	for dsk,val in pairs(res)
+	do
+		Marcel.MQTTPublish( topic .. dsk, val)
+	end
+end
