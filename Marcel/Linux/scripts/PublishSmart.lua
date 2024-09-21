d34fb3baee333a08b695288b6f1301dcd4bccd90
destroyedlolo <l_faillie@yahoo.com>
2024-09-21 19:34:31 +0200

    Add Smart reporting

---
+++ Marcel/Linux/scripts/PublishSmart.lua
@@ -0,0 +1,17 @@
+-- Publish S.M.A.R.T alert
+-- 
+--
+
+
+function PublishSmart( section_name )
+	local f = io.open('/tmp/smartd.msg','r')
+
+	if f then
+print "ok"
+		local msg = f:read('*all')
+		f:close()
+
+		local topic = 'Machines/' .. Marcel.Hostname() ..'/SMART'
+		Marcel.MQTTPublish( topic, msg )
+	end
+end
