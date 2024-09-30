419d7e0c5f257dcf181f211b2b9b1b3ef9d5d6c9
destroyedlolo <l_faillie@yahoo.com>
2024-10-01 00:54:59 +0200

    Script working

--- Majordome/50_HomeTemperatures/FeedHomeTemperatures.lua
+++ Majordome/50_HomeTemperatures/FeedHomeTemperatures.lua
@@ -3,12 +3,33 @@
 -->> listen=HomeTemperatures
 -->> need_topic=HomeTemperatures
 
-local _, nlevel=HomeTemperatures:getTopic():gsub("/","")	-- amount of level
-print(MAJORDOME_TOPIC, MAJORDOME_PAYLOAD)
+-- PostgreSQL access
+-- package.path = MAJORDOME_CONFIGURATION_DIRECTORY .. "/conf/?.lua;" .. package.path
+
+local pgmoon = require "pgmoon"
+local db = pgmoon.new( {
+	database = "www",
+--	user= "toto"
+})
+
+assert(db:connect())
 
 -- Extract probe name
+local _, nlevel=HomeTemperatures:getTopic():gsub("/","")	-- amount of level
+
 MAJORDOME_TOPIC = MAJORDOME_TOPIC ..'/'
 local tbl =  {MAJORDOME_TOPIC:match((MAJORDOME_TOPIC:gsub("[^/]*/", "([^/]*)/")))}
-
 local name = tbl[nlevel+1]
-print(name, MAJORDOME_PAYLOAD)
+
+-- ignore deeper level
+-- (usually, I publish the raw/uncorrected value)
+
+if not tbl[nlevel+2] then
+	local req = string.format("INSERT INTO domestik.probe_hardware VALUES ('Maison', %s, %f, false, now() );", db:escape_literal(name), db:escape_literal(tonumber(MAJORDOME_PAYLOAD)));
+--	print(req)
+
+	local status, err = db:query(req)
+	if not status then
+		print("failed", err)
+	end
+end
--- Postgresql/README.md
+++ Postgresql/README.md
@@ -1,5 +1,5 @@
 This directory contains SQL files needed to build Domestik's database environment in a PostgreSQL database.
 
-Common.sql defines the Domestik environment and owns tables, and is MANDATORY. It has to be installed first.
+Everything is created in Domestik2 schema.
 
 *.sql declares objects for each probe. You may refine them to the ones you're using ... (obviously, it's harmless to install everything).
