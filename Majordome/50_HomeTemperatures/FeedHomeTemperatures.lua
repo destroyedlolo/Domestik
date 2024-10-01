9b58aca834089785e50a3c145356ea79acc69e99
destroyedlolo <l_faillie@yahoo.com>
2024-10-01 22:10:47 +0200

    Add transcodification

--- Majordome/50_HomeTemperatures/FeedHomeTemperatures.lua
+++ Majordome/50_HomeTemperatures/FeedHomeTemperatures.lua
@@ -21,6 +21,20 @@ MAJORDOME_TOPIC = MAJORDOME_TOPIC ..'/'
 local tbl =  {MAJORDOME_TOPIC:match((MAJORDOME_TOPIC:gsub("[^/]*/", "([^/]*)/")))}
 local name = tbl[nlevel+1]
 
+-- Transcodage of some name
+-- If the value is nul, the topic is ignored
+
+local transco =  {
+	["GarageP"] = "Porte Garage",
+	["Congelateur"] = 0
+}
+
+if transco[name] == 0 then
+	return
+elseif transco[name] then
+	name = transco[name]
+end
+
 -- ignore deeper level
 -- (usually, I publish the raw/uncorrected value)
 
@@ -29,6 +43,11 @@ if not tbl[nlevel+2] then
 --	print(req)
 
 	local status, err = db:query(req)
+	if not status then
+		print("failed", err)
+	end
+
+	status, err = db:query("UPDATE domestik.users SET last_activity = now() WHERE id = 'Maison'" )
 	if not status then
 		print("failed", err)
 	end
