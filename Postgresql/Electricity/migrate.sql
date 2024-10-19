eb64af3a00d7148a24617c9d248eeb90d1d45f06
destroyedlolo <l_faillie@yahoo.com>
2024-10-19 23:45:01 +0200

    Migrate production as well

--- Postgresql/Electricity/migrate.sql
+++ Postgresql/Electricity/migrate.sql
@@ -17,3 +17,11 @@ INSERT INTO :Domestik_Schema.electricity_counter
 	SELECT 'Consommation', regexp_replace(probe, '^consomation_', ''), value, sample_time
 	FROM domestik.probe_counter
 	WHERE probe LIKE 'consomation_%';
+
+\qecho Migrate Consommation
+\qecho --------------------
+
+INSERT INTO :Domestik_Schema.electricity_counter
+	SELECT 'Production', regexp_replace(probe, '^production_', ''), value, sample_time
+	FROM domestik.probe_counter
+	WHERE probe LIKE 'production_%';
