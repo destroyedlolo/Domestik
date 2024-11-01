4022a3c67bfc1c88534bf017af491eba6e06b235
destroyedlolo <l_faillie@yahoo.com>
2024-11-01 23:56:28 +0100

    Handle consumer's power

---
+++ Majordome/50_Electricity/ConsPowerCollector.lua
@@ -0,0 +1,44 @@
+-- Collect consumer power statistics
+-- Store in the database the maximum collected
+--
+-- Frequency of data collection
+-->> when=5minutes
+--
+-->> need_minmax=ConsPower
+-->> need_topic=DomestikAvgConsPower
+--
+
+if MAJORDOME_VERBOSE then
+	print ""
+	print "Cons Power"
+	print "----------"
+	print( "Number of samples :", ConsPower:getSamplesNumber() )
+	print( "Min value :", ConsPower:getMin() )
+	print( "Max value :", ConsPower:getMax() )
+	print( "Average :", ConsPower:getAverage() )
+	print( "Sum :", ConsPower:getSum() )
+end
+
+-- PostgreSQL access
+package.path = MAJORDOME_CONFIGURATION_DIRECTORY .. "/conf/?.lua;" .. package.path
+
+local pgmoon = require "pgmoon"
+local DB = require("DB")
+local db = pgmoon.new(
+	DB
+)
+assert(db:connect())
+
+-- Store in database
+local req = string.format("INSERT INTO ".. DB.schema ..".electricity_power VALUES ('Consommation', %f, now() );", db:escape_literal(ConsPower:getMax(v)))
+
+local status, err = db:query(req)
+if not status then
+	print("failed", err)
+end
+
+-- Publish collected average
+DomestikAvgConsPower:Publish( ConsPower:getAverage() )
+
+-- Clear the statistics
+ConsPower:Reset()
