252f32e6e58c6bb72ab71785842c26112c7a41a9
destroyedlolo <l_faillie@yahoo.com>
2024-11-02 00:07:57 +0100

    Handle producer power

---
+++ Majordome/50_Electricity/ProdPowerCollector.lua
@@ -0,0 +1,44 @@
+-- Collect producer power statistics
+-- Store in the database the maximum collected
+--
+-- Frequency of data collection
+-->> when=5minutes
+--
+-->> need_minmax=ProdPower
+-->> need_topic=DomestikAvgProdPower
+--
+
+if MAJORDOME_VERBOSE then
+	print ""
+	print "Prod Power"
+	print "----------"
+	print( "Number of samples :", ProdPower:getSamplesNumber() )
+	print( "Min value :", ProdPower:getMin() )
+	print( "Max value :", ProdPower:getMax() )
+	print( "Average :", ProdPower:getAverage() )
+	print( "Sum :", ProdPower:getSum() )
+end
+
+-- PostgreSQL access
+package.path = MAJORDOME_CONFIGURATION_DIRECTORY .. "/conf/?.lua;" .. package.path
+
+local pgmoon = require "pgmoon"
+local DB = require("DB")
+local db = pgmoon.new(
+	DB
+)
+assert(db:connect())
+
+-- Store in database
+local req = string.format("INSERT INTO ".. DB.schema ..".electricity_power VALUES ('Production', %f, now() );", db:escape_literal(ProdPower:getMax(v)))
+
+local status, err = db:query(req)
+if not status then
+	print("failed", err)
+end
+
+-- Publish collected average
+DomestikAvgProdPower:Publish( ProdPower:getAverage() )
+
+-- Clear the statistics
+ProdPower:Reset()
