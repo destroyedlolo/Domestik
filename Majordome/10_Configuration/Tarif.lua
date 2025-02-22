9e6de1024f71dd37dd765c95f95d4502fcf88ef0
destroyedlolo <l_faillie@yahoo.com>
2025-02-22 20:34:08 +0100

    Temperature injection is working

--- Majordome/10_Configuration/Tarif.lua
+++ Majordome/10_Configuration/Tarif.lua
@@ -4,5 +4,3 @@
 SelSharedVar.Set("THP", 0.27/1000)
 SelSharedVar.Set("THC", 0.2068/1000)
 SelSharedVar.Set("TREVENTE", 0.1269/1000)
-
-SelSharedVar.dump()
--- Postgresql/Home/create_HomeFigures.sql
+++ Postgresql/Home/create_HomeFigures.sql
@@ -15,27 +15,22 @@ SET ROLE :Domestik_User;
 
 \qecho Drop potential existing environment
 \qecho -----------------------------------
-DROP TABLE IF EXISTS :Domestik_Schema.home_figures CASCADE;
-DROP TYPE IF EXISTS :Domestik_Schema.home_ekind;
+DROP TABLE IF EXISTS :Domestik_Schema.home_temperatures CASCADE;
 
-\qecho Create home_figures schema
-\qecho --------------------------
+\qecho Create home_temperatures schema
+\qecho -------------------------------
 
-CREATE TYPE :Domestik_Schema.home_ekind AS ENUM('Temperature', 'Humidity');
-COMMENT ON TYPE :Domestik_Schema.home_ekind IS 'Kind of stored data';
-
-CREATE TABLE :Domestik_Schema.home_figures (
+CREATE TABLE :Domestik_Schema.home_temperatures (
+	sample_time TIMESTAMP WITH TIME ZONE,
 	figure TEXT NOT NULL,
-	kind :Domestik_Schema.home_ekind NOT NULL,
-	value REAL,
-	sample_time TIMESTAMP WITH TIME ZONE
+	value REAL
 );
 
-GRANT ALL PRIVILEGES ON TABLE :Domestik_Schema.home_figures TO :Domestik_User;
+GRANT ALL PRIVILEGES ON TABLE :Domestik_Schema.home_temperatures TO :Domestik_User;
 
-COMMENT ON TABLE :Domestik_Schema.home_figures IS 'Home''s environmentals';
-COMMENT ON COLUMN :Domestik_Schema.home_figures.figure IS 'Figure''s name';
+COMMENT ON TABLE :Domestik_Schema.home_temperatures IS 'Home''s temperatures';
+COMMENT ON COLUMN :Domestik_Schema.home_temperatures.figure IS 'Figure''s name';
 COMMENT ON COLUMN :Domestik_Schema.home_figures.sample_time IS 'When the data sampled';
 
-CREATE INDEX dmkhff ON :Domestik_Schema.home_figures (figure);
-CREATE INDEX dmkhffs ON :Domestik_Schema.home_figures (figure, sample_time);
+CREATE INDEX dmkhff ON :Domestik_Schema.home_temperatures (figure);
+CREATE INDEX dmkhffs ON :Domestik_Schema.home_temperatures (figure, sample_time);
