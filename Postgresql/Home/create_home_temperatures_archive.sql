fed3445fd1d86a4b4a1789955c1a74c7d8325023
destroyedlolo <l_faillie@yahoo.com>
2025-05-15 23:03:13 +0200

    Correct table definition

--- Postgresql/Home/create_home_temperatures_archive.sql
+++ Postgresql/Home/create_home_temperatures_archive.sql
@@ -14,24 +14,24 @@ SET ROLE :Domestik_User;
 
 \qecho Drop potential existing environment
 \qecho -----------------------------------
-DROP TABLE IF EXISTS :Domestik_Schema.home_temperature_archives CASCADE;
+DROP TABLE IF EXISTS :Domestik_Schema.home_temperatures_archive CASCADE;
 
-\qecho Create home_temperature_archives schema
+\qecho Create home_temperatures_archive schema
 \qecho -------------------------------
 
-CREATE TABLE :Domestik_Schema.home_temperature_archives (
+CREATE TABLE :Domestik_Schema.home_temperatures_archive (
 	sample_time TIMESTAMP WITH TIME ZONE,
 	figure TEXT NOT NULL,
-	minimum INTEGER,
-	maximum INTEGER,
-	average INTEGER
+	minimum REAL,
+	maximum REAL,
+	average REAL
 );
 
-GRANT ALL PRIVILEGES ON TABLE :Domestik_Schema.home_temperature_archives TO :Domestik_User;
+GRANT ALL PRIVILEGES ON TABLE :Domestik_Schema.home_temperatures_archive TO :Domestik_User;
 
-COMMENT ON TABLE :Domestik_Schema.home_temperature_archives IS 'Archiving of home temperatures';
-COMMENT ON COLUMN :Domestik_Schema.home_temperature_archives.figure IS 'Temperatures''s name';
-COMMENT ON COLUMN :Domestik_Schema.home_temperature_archives.sample_time IS 'When the data sampled';
+COMMENT ON TABLE :Domestik_Schema.home_temperatures_archive IS 'Archiving of home temperatures';
+COMMENT ON COLUMN :Domestik_Schema.home_temperatures_archive.figure IS 'Temperatures''s name';
+COMMENT ON COLUMN :Domestik_Schema.home_temperatures_archive.sample_time IS 'When the data sampled';
 
-CREATE INDEX dmkhtaf ON :Domestik_Schema.home_temperature_archives (figure);
-CREATE INDEX dmkhtafs ON :Domestik_Schema.home_temperature_archives (figure, sample_time);
+CREATE INDEX dmkhtaf ON :Domestik_Schema.home_temperatures_archive (figure);
+CREATE INDEX dmkhtafs ON :Domestik_Schema.home_temperatures_archive (figure, sample_time);
