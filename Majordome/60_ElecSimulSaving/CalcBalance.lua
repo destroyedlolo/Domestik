531db3702e468880e2665c1119feb3c28b421d57
destroyedlolo <l_faillie@yahoo.com>
2025-05-28 01:51:25 +0200

    WIP

---
+++ Majordome/60_ElecSimulSaving/CalcBalance.lua
@@ -0,0 +1,22 @@
+-->> desc=Calculate the balance b/w electricity consumption and production
+-->> group=Electricité.Economie
+--
+-->> listen=ConsPower
+-->> need_topic=ConsPower
+-->> require_topic=ProdPower
+--
+-->> need_minmax=SavingCollector
+
+local cons, prod = tonumber(ConsPower:getVal()), tonumber(ProdPower:getVal())
+local diff = prod - cons
+
+local saving, injection
+if diff >= 0 then
+	saving = cons
+	injection = diff
+else
+	saving = prod
+	injection = 0
+end
+
+SavingCollector:Push(saving)
