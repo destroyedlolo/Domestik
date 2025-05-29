2b71b4434f56eedf81a09b6ed3e42835007e2344
destroyedlolo <l_faillie@yahoo.com>
2025-05-29 14:57:20 +0200

    change tarif

--- Majordome/10_Configuration/Tarif.lua
+++ Majordome/10_Configuration/Tarif.lua
@@ -1,6 +1,6 @@
 -->> desc=Defines Electricity's prices
 -->> RunAtStartup
 
-SelSharedVar.Set("THP", 0.27/1000)
-SelSharedVar.Set("THC", 0.2068/1000)
+SelSharedVar.Set("THP", 0.2146/1000)
+SelSharedVar.Set("THC", 0.1696/1000)
 SelSharedVar.Set("TREVENTE", 0.04/1000)
