dd999fac707108083fff6b74bc6f70881450d860
destroyedlolo <l_faillie@yahoo.com>
2025-06-02 02:10:21 +0200

    wip

--- Majordome/50_ElecConsPower/ConsPowerCollector.lua
+++ Majordome/50_ElecConsPower/ConsPowerCollector.lua
@@ -3,9 +3,14 @@
 --
 -- Frequency of data collection
 -->> when=Periode
+-->> need_timer=Periode
 --
 -- Where data come
 -->> need_namedminmax=ConsPower
 --
 
-SelLog.Log('I', "Consommation :" .. ConsPower:getAverage("Consommation"))
+SelLog.Log('I', "Consommation :" .. ConsPower:getAverage("Consommation") .. " -> ".. ConsPower:getAverage("Consommation") * (Periode:getEvery() / 3600))
+
+print(ConsPower:getSamplesNumber("Consommation"), ConsPower:getSum("Consommation"), ConsPower:getSum("Consommation") / ConsPower:getSamplesNumber("Consommation"))
+ConsPower:Reset("Consommation")
+print(ConsPower:getSamplesNumber("Consommation"))
